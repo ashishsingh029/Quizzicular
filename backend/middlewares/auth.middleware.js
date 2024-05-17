a24c9c7e2d3625bc428982ada3cfdafe6c4cfab6
Ashish Kumar Singh <162873962+ashishsingh029@users.noreply.github.com>
2024-05-17 17:56:31 +0530

    wsomr

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken')
 const authenticateUser = (req, res, next) => {
-    // const token = req.header('Authorization')
-    // if(!token){
-    //     return res.status(401).json({"message": "Unauthorized User", "reason": "Token Unavailable"})
-    // }
-    // try {
-    //     let decodedData = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET)
-    //     console.log('Decoded data: ', decodedData)
-    //     req.user = decodedData.user
-    //     next()
-    // } catch (error) {
-    //     res.status(400).json({"message": "Login Invalidated, Try Logging in Again"})
-    // }
+    const token = req.header('Authorization')
+    if(!token){
+        return res.status(401).json({"message": "Unauthorized User", "reason": "Token Unavailable"})
+    }
+    try {
+        let decodedData = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET)
+        console.log('Decoded data: ', decodedData)
+        req.user = decodedData.user
+        next()
+    } catch (error) {
+        res.status(400).json({"message": "Login Invalidated, Try Logging in Again"})
+    }
     next()
 }
 module.exports = authenticateUser
